1422c8f3fc4792a3d7088cf1cb39a6824ba4750b
Jesse van der Pluijm <jessevdp@hey.com>
2024-12-03 11:53:50 +0100

    03.2: Prepare ProgramParser for parsing multiple types of instructions

--- day_3/src/program_parser.cr
+++ day_3/src/program_parser.cr
@@ -1,19 +1,36 @@
 class ProgramParser
   def initialize(@lines = [] of String)
+    @rules = [
+      ProgramParser::Rule.create(/mul\((?<a>\d+),(?<b>\d+)\)/) do |match|
+        a = match["a"].to_i
+        b = match["b"].to_i
+        Program::MulInstruction.new(a, b)
+      end
+    ]
   end
 
   def parse
     instructions = [] of Program::Instruction
 
     @lines.each do |line|
-      matches = line.scan(/mul\((?<a>\d+),(?<b>\d+)\)/)
-      matches.each do |match|
-        a = match["a"].to_i
-        b = match["b"].to_i
-        instructions << Program::MulInstruction.new(a, b)
+      @rules.each do |rule|
+        matches = line.scan(rule.regex)
+        matches.each do |match|
+          instructions << rule.block.call(match)
+        end
       end
     end
 
     Program.new(instructions)
+  end
+end
+
+class ProgramParser::Rule
+  getter regex, block
+  def initialize(@regex : Regex, @block : Regex::MatchData -> Program::Instruction)
+  end
+
+  def self.create(regex : Regex, &block : Regex::MatchData -> Program::Instruction)
+    new(regex, block)
   end
 end
