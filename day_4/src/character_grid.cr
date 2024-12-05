83747fce95057a34cfb765f5ef7e1ccf96591e5e
Jesse van der Pluijm <jessevdp@hey.com>
2024-12-05 07:47:06 +0100

    04.1: Introduce CharacterGrid::Coordinate

--- day_4/src/character_grid.cr
+++ day_4/src/character_grid.cr
@@ -8,10 +8,19 @@ class CharacterGrid
   end
 
   def cell_at(x, y)
-    return if x < 0 || x >= width
-    return if y < 0 || y >= height
+    cell_at(CharacterGrid::Coordinate.new(x, y))
+  end
 
-    @rows[y][x]
+  def cell_at(coordinate)
+    return unless contains?(coordinate)
+
+    @rows[coordinate.y][coordinate.x]
+  end
+
+  def contains?(coordinate)
+    return false if coordinate.x < 0 || coordinate.x >= width
+    return false if coordinate.y < 0 || coordinate.y >= height
+    true
   end
 
   def width
@@ -22,6 +31,13 @@ class CharacterGrid
 
   def height
     @rows.size
+  end
+end
+
+class CharacterGrid::Coordinate
+  getter x : Int32
+  getter y : Int32
+  def initialize(@x, @y)
   end
 end
 
