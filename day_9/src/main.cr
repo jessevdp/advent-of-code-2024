db3e2b062037e104b78fc59eced59a0754750879
Jesse van der Pluijm <jessevdp@hey.com>
2024-12-09 16:56:30 +0100

    09.1: Solve part 1

--- day_9/src/main.cr
+++ day_9/src/main.cr
@@ -2,3 +2,16 @@ require "./disk"
 require "./disk_map_parser"
 require "./disk_compactor"
 
+line = if ARGV.size > 0
+  filepath = ARGV.first
+  File.read_lines(filepath).first
+else
+  ""
+end
+
+disk = Disk.from_disk_map(line)
+
+puts "Part 1:"
+disk.compact
+puts disk.checksum
+
