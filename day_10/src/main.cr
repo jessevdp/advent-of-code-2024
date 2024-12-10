0e54f8d8caf14c32a1849172775951b34b87e966
Jesse van der Pluijm <jessevdp@hey.com>
2024-12-10 13:08:30 +0100

    10.1: Solve part 1

--- day_10/src/main.cr
+++ day_10/src/main.cr
@@ -1,3 +1,15 @@
 require "./point"
 require "./map"
 
+lines = if ARGV.size > 0
+  filepath = ARGV.first
+  File.read_lines(filepath)
+else
+  [] of String
+end
+
+map = Map.from_input(lines)
+
+puts "Part 1:"
+puts map.trailheads.map { |point| point.reachable_trailends.size }.sum
+
