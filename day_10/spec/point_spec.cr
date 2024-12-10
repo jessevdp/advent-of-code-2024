411c555941bfd026ded1f791418d3431c3bf2bdf
Jesse van der Pluijm <jessevdp@hey.com>
2024-12-10 13:06:54 +0100

    10.1: Add Point#reachable_trailends

--- day_10/spec/point_spec.cr
+++ day_10/spec/point_spec.cr
@@ -12,5 +12,20 @@ describe Point do
       point.reachable_neighbours.should eq([a, d])
     end
   end
+
+  describe "#reachable_trailends" do
+    it "returns all trailend Points that are reachable through #reachable_neighbours recursively" do
+      map = Map.from_input([
+        "0123",
+        "1234",
+        "8765",
+        "9876",
+      ])
+      trailend = map.point!(x: 0, y: 3)
+      map.point!(x: 0, y: 0).reachable_trailends.should eq(Set{trailend})
+      map.point!(x: 0, y: 2).reachable_trailends.should eq(Set{trailend})
+      map.point!(x: 0, y: 3).reachable_trailends.should eq(Set{trailend})
+    end
+  end
 end
 
--- day_10/src/point.cr
+++ day_10/src/point.cr
@@ -10,8 +10,24 @@ class Point
     @height == 0
   end
 
+  def trailend?
+    @height == 9
+  end
+
   def reachable_neighbours
     @neighbours.select { |neighbour| (neighbour.height - @height) == 1 }
+  end
+
+  def reachable_trailends
+    return Set{self} if trailend?
+
+    trailends = Set(Point).new
+
+    reachable_neighbours.each do |neighbour|
+      trailends += neighbour.reachable_trailends
+    end
+
+    trailends
   end
 end
 
