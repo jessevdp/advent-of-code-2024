fb2d9d0d9bf9b44ef92261d9c105dbf993cacd41
Jesse van der Pluijm <jessevdp@hey.com>
2024-12-12 16:52:41 +0100

    12.1: Solve part 1

--- day_12/src/main.cr
+++ day_12/src/main.cr
@@ -2,3 +2,15 @@ require "./plot"
 require "./region"
 require "./map"
 
+lines = if ARGV.size > 0
+  filepath = ARGV.first
+  File.read_lines(filepath)
+else
+  [] of String
+end
+
+map = Map.from_input(lines)
+
+puts "Part 1:"
+puts map.regions.map { |region| region.area * region.perimeter }.sum
+
