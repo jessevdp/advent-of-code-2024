8580ed7cb1432cf99884c0534da03c2bdf7c6858
Jesse van der Pluijm <jessevdp@hey.com>
2024-12-13 17:23:41 +0100

    13.1: Solve part 1

--- day_13/src/main.cr
+++ day_13/src/main.cr
@@ -5,3 +5,20 @@ require "./combination"
 require "./claw_machine"
 require "./claw_machines_configuration_parser"
 
+lines = if ARGV.size > 0
+  filepath = ARGV.first
+  File.read_lines(filepath)
+else
+  [] of String
+end
+
+claw_machines = ClawMachinesConfigurationParser.new(lines).parse
+
+puts "Part 1:"
+puts claw_machines
+  .select(&.possible?)
+  .map(&.cheapest_combination)
+  .compact
+  .map(&.total_cost)
+  .sum
+
