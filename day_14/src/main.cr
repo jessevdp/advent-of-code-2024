93a487fc7096a0127d0b52fa1dd316fe697fcf0e
Jesse van der Pluijm <jessevdp@hey.com>
2024-12-14 11:58:46 +0100

    14.2: Solve part 2 (...)
    Using MacOS finder.

--- day_14/src/main.cr
+++ day_14/src/main.cr
@@ -23,6 +23,39 @@ def part_1(lines)
   puts robot_counts.reduce(1) { |acc, i| acc * i }
 end
 
+def draw_grid(robots, bounds)
+  lines = [] of String
+  bounds.height.times do |y|
+    line = ""
+    bounds.width.times do |x|
+      point = Point.new(x, y)
+      filled = robots.any? { |robot| robot.position == point }
+      line += filled ? 'X' : '.'
+    end
+    lines << line
+  end
+  lines
+end
+
+def part_2(lines)
+  bounds = BoundingBox.of_dimensions(width: 101, height: 103)
+  robots = lines.map { |line| Robot.from_input(line, bounds) }
+
+  seconds = 0
+  100_000.times do
+    seconds += 1
+    robots.each(&.tick)
+    filepath = "./output/#{seconds}.txt"
+    File.open(filepath, "w") do |file|
+      file.puts "===================="
+      file.puts "seconds: #{seconds}"
+      file.puts "===================="
+      file.puts draw_grid(robots, bounds).join('\n')
+      file.puts ""
+    end
+  end
+end
+
 lines = if ARGV.size > 0
   filepath = ARGV.first
   File.read_lines(filepath)
@@ -31,3 +64,4 @@ else
 end
 
 part_1(lines)
+part_2(lines)
